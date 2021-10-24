12055e66be055f1d8bc6ac2038caef23c2eed774
Shreyash Y Patil <shreyashypatil1122002@gmail.com>
2021-10-24 22:27:08 +0530

    Method overloading

---
+++ src/UseMethodOver1.java
@@ -0,0 +1,10 @@
+public class UseMethodOver1
+{
+    public static void main(String[] args) {
+        MethodOver1 m= new MethodOver1();
+        m.show('E');
+        m.show(12);
+        m.show("Hello");
+        m.show(2.4f);
+    }
+}
